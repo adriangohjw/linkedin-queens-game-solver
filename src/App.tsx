459c70806aa8f6b39bc3e68039974821589f5acd
adriangohjw <adriangohjw@gmail.com>
2024-10-03 02:00:30 +0800

    feat(App): add puzzle color state management and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { COLOR_OPTIONS } from "./constant";
+import INITIAL_PUZZLE_COLOR from "./InitialPuzzleColor";
 import StartingLayout from "./StartingLayout";
 import SolvedLayout from "./SolvedLayout";
 import Footer from "./Footer";
@@ -5,12 +8,43 @@ import Footer from "./Footer";
 export default function App() {
   const size = 8;
 
+  const [puzzleColors, setPuzzleColors] =
+    useState<(string | null)[][]>(INITIAL_PUZZLE_COLOR);
+
+  function setPuzzleColor({
+    row,
+    col,
+    color,
+  }: {
+    row: number;
+    col: number;
+    color: string | null;
+  }) {
+    setPuzzleColors((prev) => {
+      const newGrid = [...prev];
+      newGrid[row][col] = color;
+      return newGrid;
+    });
+  }
+
+  let colors: Record<string, { row: number; col: number }[]> =
+    Object.fromEntries(COLOR_OPTIONS.map((color) => [color, []]));
+
+  for (let row = 0; row < size; row++) {
+    for (let col = 0; col < size; col++) {
+      const color = puzzleColors[row][col];
+      if (color) {
+        colors[color].push({ row, col });
+      }
+    }
+  }
+
   return (
     <div className="text-center m-4 p-4">
       <h1 className="text-3xl font-bold">LinkedIn Queens Game Solver</h1>
       <div className="flex flex-row justify-center items-center mt-4">
-        <StartingLayout size={size} />
-        <SolvedLayout size={size} />
+        <StartingLayout size={size} puzzleColors={puzzleColors} />
+        <SolvedLayout size={size} puzzleColors={puzzleColors} />
       </div>
       <Footer />
     </div>
