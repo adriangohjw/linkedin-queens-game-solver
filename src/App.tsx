93fe2c713fc62189c83d27d26bb8649b48b58602
adriangohjw <adriangohjw@gmail.com>
2024-10-02 21:14:21 +0800

    feat: add SolvedLayout component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css';
-import StartingLayout from './StartingLayout';
+import "./App.css";
+import StartingLayout from "./StartingLayout";
+import SolvedLayout from "./SolvedLayout";
 
 export default function App() {
   return (
     <div className="App">
       <StartingLayout />
+      <SolvedLayout />
     </div>
   );
 }
---
+++ src/SolvedLayout.tsx
@@ -0,0 +1,7 @@
+export default function SolvedLayout() {
+  return (
+    <div>
+      <h1>Solved Layout</h1>
+    </div>
+  );
+}
