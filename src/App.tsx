7b792d9592ebb575a82dc8a8128ece8c1535a08d
adriangohjw <adriangohjw@gmail.com>
2024-10-02 21:27:53 +0800

    refactor: update styling in App and Layout components

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import SolvedLayout from "./SolvedLayout";
 
 export default function App() {
   return (
-    <div className="text-center">
-      <h1 className="text-3xl font-bold text-blue-600">
-        LinkedIn Queens Game Solver
-      </h1>
-      <div className="flex flex-row justify-center items-center">
+    <div className="text-center m-4 p-4">
+      <h1 className="text-3xl font-bold">LinkedIn Queens Game Solver</h1>
+      <div className="flex flex-row justify-center items-center mt-4">
         <StartingLayout />
         <SolvedLayout />
       </div>
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,7 +1,7 @@
 export default function Layout({ title }: { title: string }) {
   return (
     <div className="flex flex-col justify-center items-center border border-lightgrey p-2 m-2 w-1/2 rounded-lg">
-      <h2 className="text-2xl font-bold text-center text-blue-600">{title}</h2>
+      <h2 className="text-2xl font-bold text-center">{title}</h2>
     </div>
   );
 }
