28352f1be0e7a6cdcd348bda5c448e21688672c7
adriangohjw <adriangohjw@gmail.com>
2024-10-04 16:21:27 +0800

    refactor: separate fillColor logic for row and column handling

--- src/puzzleUtils/fillColorInSingleRowOrCol.tsx
+++ src/puzzleUtils/fillColorInSingleRowOrCol.tsx
@@ -3,6 +3,66 @@ import duplicatePuzzleContent from "./duplicatePuzzleContent";
 import { markNo } from "./markUtils";
 import { getRowIndices, getColIndices } from "./util";
 
+const fillColorInSingleRow = ({
+  size,
+  color,
+  puzzleContent,
+  puzzleColors,
+  emptyCells,
+}: {
+  size: number;
+  color: ColorType;
+  puzzleContent: CellContentType[][];
+  puzzleColors: ColorType[][];
+  emptyCells: CellType[];
+}): CellContentType[][] => {
+  const rowIndices = getRowIndices({ cells: emptyCells });
+  if (rowIndices.size === 1) {
+    const row = rowIndices.values().next().value;
+    if (typeof row === "number") {
+      for (let i = 0; i < size; i++) {
+        if (puzzleColors[row][i] !== color) {
+          puzzleContent = markNo({
+            puzzleContent,
+            cell: { row, col: i } as CellType,
+          });
+        }
+      }
+    }
+  }
+  return puzzleContent;
+};
+
+const fillColorInSingleCol = ({
+  size,
+  color,
+  puzzleContent,
+  puzzleColors,
+  emptyCells,
+}: {
+  size: number;
+  color: ColorType;
+  puzzleContent: CellContentType[][];
+  puzzleColors: ColorType[][];
+  emptyCells: CellType[];
+}): CellContentType[][] => {
+  const colIndices = getColIndices({ cells: emptyCells });
+  if (colIndices.size === 1) {
+    const col = colIndices.values().next().value;
+    if (typeof col === "number") {
+      for (let i = 0; i < size; i++) {
+        if (puzzleColors[i][col] !== color) {
+          puzzleContent = markNo({
+            puzzleContent,
+            cell: { row: i, col } as CellType,
+          });
+        }
+      }
+    }
+  }
+  return puzzleContent;
+};
+
 const fillColorInSingleRowOrCol = ({
   color,
   puzzleContent,
@@ -20,35 +80,21 @@ const fillColorInSingleRowOrCol = ({
     puzzleContent,
   });
 
-  const rowIndices = getRowIndices({ cells: emptyCells });
-  if (rowIndices.size === 1) {
-    const row = rowIndices.values().next().value;
-    if (typeof row === "number") {
-      for (let i = 0; i < size; i++) {
-        if (puzzleColors[row][i] !== color) {
-          newPuzzleContent = markNo({
-            puzzleContent: newPuzzleContent,
-            cell: { row, col: i } as CellType,
-          });
-        }
-      }
-    }
-  }
+  newPuzzleContent = fillColorInSingleRow({
+    size,
+    color,
+    puzzleContent: newPuzzleContent,
+    puzzleColors,
+    emptyCells,
+  });
 
-  const colIndices = getColIndices({ cells: emptyCells });
-  if (colIndices.size === 1) {
-    const col = colIndices.values().next().value;
-    if (typeof col === "number") {
-      for (let i = 0; i < size; i++) {
-        if (puzzleColors[i][col] !== color) {
-          newPuzzleContent = markNo({
-            puzzleContent: newPuzzleContent,
-            cell: { row: i, col } as CellType,
-          });
-        }
-      }
-    }
-  }
+  newPuzzleContent = fillColorInSingleCol({
+    size,
+    color,
+    puzzleContent: newPuzzleContent,
+    puzzleColors,
+    emptyCells,
+  });
 
   return newPuzzleContent;
 };
